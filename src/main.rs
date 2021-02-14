e7f45df4dfecd986b62f80df57a29c3e44b23c6e
Oliver Downard <olivermd@btinternet.com>
2021-02-14 21:23:55 +0000

    adds rounded lines

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use seed::{prelude::*, *};
 fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
     Model {
         draw_context: DrawContext::new(1000_f64 / 4_f64, 1000_f64, 2000_f64, 4, 6),
-        lines: vec![((1, 0), (3, 4))],
+        lines: vec![],
         show_points: true,
         x_limits: (0, 4),
         y_limits: (0, 6),
@@ -103,9 +103,22 @@ fn view(model: &Model) -> Node<Msg> {
                     },
                     ev(Ev::Click, |_| Msg::ToggleShowPoints)
                 ],
-                button!["Next", ev(Ev::Click, |_| Msg::NextRandomLine),],
+                button![
+                    "Next",
+                    style! {
+                    St::Display => "flex",
+                    St::JustifyContent => "center",
+                    St::AlignItems => "center",
+                    },
+                    ev(Ev::Click, |_| Msg::NextRandomLine),
+                ],
                 button![
                     "Add",
+                    style! {
+                    St::Display => "flex",
+                    St::JustifyContent => "center",
+                    St::AlignItems => "center",
+                    },
                     attrs! {At::Disabled => model.next_line.is_none().as_at_value()},
                     ev(Ev::Click, |_| Msg::AddLine),
                 ]
@@ -142,15 +155,25 @@ fn view(model: &Model) -> Node<Msg> {
 fn draw_line(
     ((from_x, from_y), (to_x, to_y)): ((i16, i16), (i16, i16)),
     ctx: &DrawContext,
-) -> Node<Msg> {
+) -> Vec<Node<Msg>> {
+    let mut ret = Vec::new();
+
     let from_x = ctx.x_spacing * (from_x + 1) as f64;
     let from_y = ctx.y_spacing * (from_y + 1) as f64;
     let to_x = ctx.x_spacing * (to_x + 1) as f64;
     let to_y = ctx.y_spacing * (to_y + 1) as f64;
 
-    line_![
+    ret.push(line_![
         attrs! {At::X1 => from_x, At::Y1 => from_y, At::X2 => to_x, At::Y2 => to_y, At::Stroke => "black", At::StrokeWidth => 20}
-    ]
+    ]);
+    ret.push(circle![
+        attrs! {At::Cx => from_x, At::Cy => from_y, At::R => 10}
+    ]);
+    ret.push(circle![
+        attrs! {At::Cx => to_x, At::Cy => to_y, At::R => 10}
+    ]);
+
+    ret
 }
 
 fn gen_circles(ctx: &DrawContext) -> Vec<Node<Msg>> {
